43dea136ba8a67d39f110d33abc688596bfcbc5f
Nick Nelson <contact@nicknelson.io>
2016-06-03 09:52:10 -0700

    Added not yet implemented error for index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var Ziplink = require('../models/Ziplink.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('error', { message: 'Not yet implemented' });
+});
+
+/* Ziplink display page */
+router.get('/#*', function(req, res, next) {
+
+	//TODO: replace placeholder with code to query DB
+	var ziplinkData = "Placeholder";
+
+	res.render('ziplink', { ziplinkData: ziplinkData });
 });
 
 module.exports = router;
