5226da4ba8c4acfef8a48e70d041871ebd2e8221
Nick Nelson <contact@nicknelson.io>
2016-06-04 00:53:25 -0700

    Implemented auto-generation of ziplinkID if none is specified in models/Ziplink

--- models/Ziplink.js
+++ models/Ziplink.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/ziplink');
 var Schema = mongoose.Schema;
 
+var shortid = require('shortid');
+
 //setup Ziplink schema
 var ziplinkSchema = new Schema({
 	ziplinkID: {
 		type: String,
+		'default': shortid.generate,
 		required: true,
 		minlength: [1, 'Empty ziplinkID'],
 		maxlength: [64, 'ziplinkID too long']
@@ -30,22 +33,23 @@ ziplinkSchema.statics.findByID = function (linkID, callback){
  */
 ziplinkSchema.statics.createZiplinkFromTemplate = function (ziplinkTemplate, callback){
 
+	//If ziplinkTemplate has a blank ziplinkID, remove it to make way for a generated default
+	if(ziplinkTemplate.ziplinkID == '')
+		delete ziplinkTemplate.ziplinkID;
+
 	var newZiplink = new this(ziplinkTemplate);
 
-	if(typeof newZiplink.ziplinkID != 'undefined'){
-		//a ziplinkID has been provided, check to make sure it doesn't already exist
-		this.findByID(newZiplink.ziplinkID, function(err, matchingZiplink){
-			if(err){ //check if there was an error
-				callback(err);
-			} else if(matchingZiplink != null){ //make sure we aren't doubling up on IDs
-				callback('ziplink with this ID already exists');
-			} else {
-				newZiplink.save(function(err){
-					callback(err, newZiplink);
-				});
-			}
-		});
-	}
+	this.findByID(newZiplink.ziplinkID, function(err, matchingZiplink){
+		if(err){ //check if there was an error
+			callback(err);
+		} else if(matchingZiplink != null){ //make sure we aren't doubling up on IDs
+			callback('ziplink with this ID already exists');
+		} else {
+			newZiplink.save(function(err){
+				callback(err, newZiplink);
+			});
+		}
+	});
 };
 
 var Ziplink = mongoose.model('Ziplink', ziplinkSchema);
