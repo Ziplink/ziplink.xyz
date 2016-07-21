782ae39b82d2d5fd6eb7a07b63205d4f306e7fdf
aussieshibe <nnelson263@gmail.com>
2016-07-21 08:56:38 +0000

    Updated all reference to pre 0.1 API functions

--- routes/debug.js
+++ routes/debug.js
@@ -11,10 +11,16 @@ var router = express.Router();
 var Ziplink = require('ziplink-basic-mongo-storage');
 
 router.get('/all', function(req, res, next){
-	Ziplink.find({}, function(err, ziplinks){
+  
+  var err = new Error("Not Yet Implemented");
+  next(err);
+  
+  /* Relies on functionality not present in Ziplink Storage API 0.1
+	Ziplink.findByID({}, function(err, ziplinks){
 		res.render('debug/all', { ziplinks: ziplinks });
 		next(err);
 	});
+	*/
 });
 
 
