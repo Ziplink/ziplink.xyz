9f8e6fdf780d4c8acec4954f6fde4ff771885d17
aussieshibe <nnelson263@gmail.com>
2016-07-24 10:29:56 +0000

    Revert "Added view to test edit functionality"
    This reverts commit dd7a998bd86cb5f85cf9cf2813acb74f70fe7f05.

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,8 @@ var Ziplink = require('ziplink-basic-mongo-storage');
 
 var newRoute = require('./new.js');
 var debugRoute = require('./debug.js');
-var editRoute = require('./edit.js');
 
 router.use('/~new', newRoute);
-router.use('/', editRoute);
 
 //If NODE_ENV is undefined or 'development', use the debug route
 if ((process.env.NODE_ENV || 'development') == 'development')
